05e0c0d2e681c1dc0820a9f200786dee2e1fbe0f
Alimiyan <alimiyan123@gmail.com>
2021-08-10 06:31:11 -0700

    signup from password stored and hash generated

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
+var userHelpers = require('../helpers/user-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,4 +15,14 @@ router.get('/login',(req,res)=>{
   res.render('user/login')
 })
 
+//signup page
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response)
+  })
+})
 module.exports = router;
