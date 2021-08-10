3a556ead1423d5e1aa251f582e1fe7643c4509c4
Alimiyan <alimiyan123@gmail.com>
2021-08-10 05:23:06 -0700

    created login page

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,13 @@ var productHelpers = require('../helpers/product-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products', { title: 'ECommerce', products, admin: false});
+    res.render('user/view-products', { title: 'ECommerce', products});
   })
 });
+
+//login page
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
 
 module.exports = router;
