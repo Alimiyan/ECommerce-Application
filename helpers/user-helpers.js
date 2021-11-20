df21620f0a3af750d23b68dbcf8b0ffdb3eab361
alimiyan <alimiyan123@gmail.com>
2021-11-20 16:27:48 +0530

    order placement

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -164,6 +164,49 @@ module.exports ={
                 resolve(true)
             })
         })
+    },
+
+    getTotalAmount:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let total= await db.get().collection(collection.CART_COLLECTIONS).aggregate([
+                {
+                    $match:{user:objectId(userId)}
+                },
+                {
+                    $unwind:'$products'
+                },
+                {
+                    $project:{
+                        item:'$products.item',
+                        quantity:'$products.quantity'
+                    }
+                },
+                {
+                    $lookup:{
+                        from:collection.PRODUCT_COLLECTIONS,
+                        localField: 'item',
+                        foreignField: '_id',
+                        as: 'products'
+                    }
+                },
+                {
+                    $project:{
+                        item:1,
+                        quantity:1,
+                        products:{$arrayElemAt:['$products',0]}
+                    }
+                },
+                {
+                    $group:{
+                        _id:null,
+                        total:{$sum:{$multiply:['$quantity',{$convert:{input:'$products.Price',to:'int'}}]}}
+                    }
+                }
+            ]).toArray()
+            console.log(total)
+            resolve(total[0].total)
+        })
+
     }
     
 }   
--- routes/user.js
+++ routes/user.js
@@ -95,4 +95,9 @@ router.post('/remove-product',(req,res,next)=>{
   })
 })
 
+router.get('/place-order',verifyLogin,async(req,res)=>{
+  let total= await userHelpers.getTotalAmount(req.session.user._id)
+  res.render('user/place-order',{total})
+})
+
 module.exports = router;
